af8e90656d61ec080fb178265b6ccae6b744d4df
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2022-07-12 22:37:26 +0530

    Merge-Sort Added

--- sorting.cpp
+++ sorting.cpp
@@ -6,7 +6,7 @@ void swap(int &a,int &b){
 	int t=a;a=b;b=t;
 }
 
-void bucketSort(int *ar,int n){
+void bubbleSort(int *ar,int n){
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n-i-1;j++){
 			if(ar[j] > ar[j+1])
@@ -40,6 +40,39 @@ void selectionSort(int *ar,int n){
 	}
 }
 
+void merge(int *ar,int start,int end,int mid){
+	int i = start,j = mid + 1,k = start;
+	int b[100];
+
+	while(i <= mid && j <= end){
+		if(ar[i] < ar[j])
+			b[k++] = ar[i++];
+		else
+			b[k++] = ar[j++];
+	}
+
+	if(i > mid){
+		while(j <= end)
+			b[k++] = ar[j++];
+	}
+	else{
+		while(i <= mid)
+			b[k++] = ar[i++];
+	}
+
+	for(k=start;k<=end;k++)
+		ar[k] = b[k];
+}
+
+void mergeSort(int *ar,int start,int end){
+	if(start < end){
+		int mid = (start + end) / 2;
+		mergeSort(ar,start,mid);
+		mergeSort(ar,mid+1,end);
+		merge(ar,start,end,mid);
+	}
+}
+
 void print(int *ar,int n){
 	for(int i=0;i<n;i++)
 		cout<<ar[i]<<" ";
@@ -57,6 +90,9 @@ int main(){
 		ar[i] = rand() % 1000;
 
 	print(ar,n);
-	bucketSort(ar,n);
+	// bubbleSort(ar,n);
+	// insertionSort(ar,n);
+	// selectionSort(ar,n);
+	mergeSort(ar,0,n-1);
 	print(ar,n);
 }
